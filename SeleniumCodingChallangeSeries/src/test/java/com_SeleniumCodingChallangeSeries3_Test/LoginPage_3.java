85067cf4f846e48edbfb42638b20b9105ee98b0a
meetparag81 <paragborawake81@gmail.com>
2019-06-09 19:04:31 +0530

    codingchallange_3 changes

---
+++ SeleniumCodingChallangeSeries/src/test/java/com_SeleniumCodingChallangeSeries3_Test/LoginPage_3.java
@@ -0,0 +1,15 @@
+package com_SeleniumCodingChallangeSeries3_Test;
+
+import org.openqa.selenium.support.PageFactory;
+
+import com_SeleniumCodingChallange_TestBase.TestBase;
+
+public class LoginPage_3 extends TestBase
+{
+	
+	LoginPage_3()
+	{
+		PageFactory.initElements(driver, this);
+	}
+
+}
