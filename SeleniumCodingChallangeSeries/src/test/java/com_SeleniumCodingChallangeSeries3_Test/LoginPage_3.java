293c28d09c54e7a824cec4d6f34c236301a5d097
meetparag81 <paragborawake81@gmail.com>
2019-06-09 19:10:17 +0530

    Login

--- SeleniumCodingChallangeSeries/src/test/java/com_SeleniumCodingChallangeSeries3_Test/LoginPage_3.java
+++ SeleniumCodingChallangeSeries/src/test/java/com_SeleniumCodingChallangeSeries3_Test/LoginPage_3.java
@@ -1,15 +1,23 @@
 package com_SeleniumCodingChallangeSeries3_Test;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com_SeleniumCodingChallange_TestBase.TestBase;
 
 public class LoginPage_3 extends TestBase
 {
+	@FindBy(xpath= )WebElement UserName;
 	
 	LoginPage_3()
 	{
 		PageFactory.initElements(driver, this);
+	}
+	
+	public void EnterValidUserName(String UserName)
+	{
+		
 	}
 
 }
